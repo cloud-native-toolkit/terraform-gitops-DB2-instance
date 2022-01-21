a3c6e2b28f60fbd557b4d0f939fa5a6861610a36
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-21 15:53:08 +0530

    added db2 operator
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

---
+++ test/stages/stage3-db2u-operator.tf
@@ -0,0 +1,9 @@
+module "gitops-db2" {  
+  source = "https://github.com/cloud-native-toolkit/terraform-gitops-db2u-operator.git"
+
+  gitops_config = module.gitops.gitops_config
+  git_credentials = module.gitops.git_credentials
+  server_name = module.gitops.server_name
+  namespace = module.gitops_namespace.name
+  kubeseal_cert = module.gitops.sealed_secrets_cert
+}
