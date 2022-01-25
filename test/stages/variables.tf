92d30dc2837b594ad0d0cb5b290438ff1542b837
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-25 21:49:32 +0530

    removed variable-2
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -91,9 +91,11 @@ variable "create_external_etcd" {
   type = bool
   default = false
 }
-variable "region" {  
-  description = "Region for VLANs defined in private_vlan_number and public_vlan_number."
-  //default     = "us-east"
+
+variable "workers" {
+  type        = number
+  description = "Number of worker nodes"
+  default     = 3
 }
 
  
