78e34ca297beaad96f55a365fa6bd7e0178b6729
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-31 18:16:03 +0530

    added secret
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- main.tf
+++ main.tf
@@ -18,6 +18,20 @@ module setup_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
+module pull_secret {
+  source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
+
+  gitops_config = var.gitops_config
+  git_credentials = var.git_credentials
+  server_name = var.server_name
+  kubeseal_cert = var.kubeseal_cert
+  namespace = var.namespace
+  docker_username = "cp"
+  docker_password = var.entitlement_key
+  docker_server   = "cp.icr.io"
+  secret_name     = "ibm-entitlement-key"
+}
+
 resource null_resource create_yaml {
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.yaml_dir}'"
