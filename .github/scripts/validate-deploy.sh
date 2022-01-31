1add3b4e8c589c500c0c7187e04b58353c23778b
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-31 22:42:03 +0530

    updated secret
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -50,7 +50,7 @@ else
   sleep 30
 fi
 sleep 5m
-DEPLOYMENT="c-db2ucluster-sam-ldap"
+DEPLOYMENT="c-db2ucluster-ldap"
 count=0
 until kubectl get deployment "${DEPLOYMENT}" -n "${NAMESPACE}" || [[ $count -eq 20 ]]; do
   echo "Waiting for deployment/${DEPLOYMENT} in ${NAMESPACE}"
