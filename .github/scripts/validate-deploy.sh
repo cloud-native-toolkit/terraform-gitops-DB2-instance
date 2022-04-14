ac26872f6054f09fff1de6222b210fa025d31dbf
Snehal Pansare <spansari@in.ibm.com>
2022-04-14 14:53:57 +0530

    Issue 11 fix
    Signed-off-by: Snehal Pansare <spansari@in.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -71,6 +71,8 @@ sleep 30
 INST_STATUS=$(kubectl get Db2oltpService db2oltp-cr -n "${CPD_NAMESPACE}" -o jsonpath='{.status.db2oltpStatus} {"\n"}')
 echo "DB2 Db2oltpService/db2oltp-cr is ${INST_STATUS}"
 
+oc delete job db2oltp-operandreg-job
+
 cd ..
 rm -rf .testrepo
 
