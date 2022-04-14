2c81d1ce065aa169ca9dbc16cc4885be36e4328e
Snehal Pansare <spansari@in.ibm.com>
2022-04-14 22:43:00 +0530

    issue 11 fix
    Signed-off-by: Snehal Pansare <spansari@in.ibm.com>

--- charts/ibm-cpd-db2-subscription/scripts/update_operandregistry.sh
+++ charts/ibm-cpd-db2-subscription/scripts/update_operandregistry.sh
@@ -2,9 +2,12 @@
 
 ZEN_OPERATORS_NAMESPACE="${ZEN_OPERATORS_NAMESPACE}"
 COMMON_SERVICES_NAMESPACE="${COMMON_SERVICES_NAMESPACE}"
+SOURCE_NAME="ibm-db2uoperator-catalog"
 
 oc get operandregistry common-service -n ${COMMON_SERVICES_NAMESPACE} -o json > /temp/operandregistry.json
 
 jq --arg ZEN_OPERATORS_NAMESPACE $ZEN_OPERATORS_NAMESPACE '(.spec.operators[] | select(.name == "ibm-db2u-operator")).namespace |= $ZEN_OPERATORS_NAMESPACE' /temp/operandregistry.json > /temp/operandregistry_new.json
 
-oc apply -f /temp/operandregistry_new.json
+jq --arg SOURCE_NAME $SOURCE_NAME '(.spec.operators[] | select(.name == "ibm-db2u-operator")).sourceName |= $SOURCE_NAME' /temp/operandregistry_new.json > /temp/operandregistry_new1.json
+
+oc apply -f /temp/operandregistry_new1.json
