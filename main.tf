315563e19f63d9bfae949b51be506527cb76bad0
Snehal Pansare <spansari@in.ibm.com>
2022-04-14 13:31:46 +0530

    Issue 11 fix
    Signed-off-by: Snehal Pansare <spansari@in.ibm.com>

--- main.tf
+++ main.tf
@@ -56,7 +56,6 @@ module setup_service_account {
 
   gitops_config = var.gitops_config
   git_credentials = var.git_credentials
-  namespace = var.cpd_namespace
   name = "db2oltp-operandreg-sa"
   server_name = var.server_name  
 }
